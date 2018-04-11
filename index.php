b25616625543ebdd0f8d8c9e5bf0b7f515d4cfbe
gerardo <gmontivero@jus.mendoza.gov.ar>
2018-04-10 22:05:15 -0300

    Mucho mas para hacer

--- index.php
+++ index.php
@@ -44,33 +44,7 @@
 
 </h1>
 
-<!-- Navigation -->
-<!--<nav class="navbar navbar-expand-lg navbar-dark py-lg-4" id="mainNav">
-  <div class="container">
-    <a class="navbar-brand text-uppercase text-expanded font-weight-bold d-lg-none" href="#">Start Bootstrap</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarResponsive">
-      <ul class="navbar-nav mx-auto">
-        <li class="nav-item active px-lg-4">
-          <a class="nav-link text-uppercase text-expanded" href="index.html">Home
-            <span class="sr-only">(current)</span>
-          </a>
-        </li>
-        <li class="nav-item px-lg-4">
-          <a class="nav-link text-uppercase text-expanded" href="about.html">About</a>
-        </li>
-        <li class="nav-item px-lg-4">
-          <a class="nav-link text-uppercase text-expanded" href="products.html">Products</a>
-        </li>
-        <li class="nav-item px-lg-4">
-          <a class="nav-link text-uppercase text-expanded" href="store.html">Store</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>-->
+
 <main class="container">
     <div class="row">
 
