1441efa1032e78251a6a111ba384b12afb9ae679
gerardo montivero <gerardo.montivero@gmail.com>
2018-04-23 11:24:00 -0300

    De nuevo el día mejora

--- gerVendor/gerFunctions.php
+++ gerVendor/gerFunctions.php
@@ -8,33 +8,56 @@
 
 date_default_timezone_set("America/Argentina/Mendoza");
 
-//$anio=date ('Y');
+$anio=date ('Y');
+
+$mes = date ('m');
+
+$dia=date('d');
+
+//$anio='2018';
 //
-//$mes = date ('m');
+//$mes = '04';
 //
-//$dia=date('d');
+//$dia='29';
 $hora = date ('h:i:s');
 
-$anio=2018;
 
-$mes=4;
 
-$dia=22;
+# Obtenemos el numero de la semana
+
+$semana=date("W",mktime(0,0,0,$mes,$dia,$anio));
+
+
 
 # Obtenemos el día de la semana de la fecha dada
 
 $diaSemana=date("w",mktime(0,0,0,$mes,$dia,$anio));
+
+
+
+# el 0 equivale al domingo...
+
+//if($diaSemana==0)
+//
+//    $diaSemana=7;
+
+
 
 # A la fecha recibida, le restamos el dia de la semana y obtendremos el lunes
 
 $primerDia=date("d-m-Y",mktime(0,0,0,$mes,$dia-$diaSemana+1,$anio));
 
 
+
 # A la fecha recibida, le sumamos el dia de la semana menos siete y obtendremos el domingo
 
 $ultimoDia=date("d-m-Y",mktime(0,0,0,$mes,$dia+(7-$diaSemana),$anio));
 
-$muestroDia = ($diaSemana == 0) ? date($anio . '/'. $mes . '/'. $dia) : $ultimoDia;
+$aa = substr($ultimoDia, 6, 4);
+$mm = substr($ultimoDia, 3, 2);
+$dd = substr($ultimoDia, 0, 2);
+
+$muestroDia = ($diaSemana == 0) ? $anio . '/'. $mes . '/'. $dia : $aa .'/'.$mm .'/'.$dd;
 
 
 
