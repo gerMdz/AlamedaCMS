06f6b0fcdf955edb873c8e69f8b04899f89fd1ef
Gerardo J. Montivero <gerardo.montivero@gmail.com>
2020-04-26 09:13:25 -0300

    hasta login con style

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,20 +4,55 @@ namespace App\Controller;
 
 use App\Entity\IndexAlameda;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
+//    /**
+//     * @Route("/login", name="app_login")
+//     */
+//    public function index()
+//    {
+//        $em = $this->getDoctrine()->getManager();
+//        $indexAlameda = $em->getRepository(IndexAlameda::class)->findAll();
+//        return $this->render('security/index.html.twig', [
+//            'datosIndex'=> $indexAlameda[0]
+//
+//        ]);
+//    }
+
     /**
-     * @Route("/login", name="app_login")
+     * @Route("/ingreso", name="app_login")
+     * @param AuthenticationUtils $authenticationUtils
+     * @return Response
      */
-    public function index()
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
+
+        // get the login error if there is one
         $em = $this->getDoctrine()->getManager();
         $indexAlameda = $em->getRepository(IndexAlameda::class)->findAll();
-        return $this->render('security/index.html.twig', [
-            'datosIndex'=> $indexAlameda[0]
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
 
-        ]);
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+            'datosIndex'=> $indexAlameda[0]
+            ]);
+    }
+
+    /**
+     * @Route("/logout", name="app_logout")
+     */
+    public function logout()
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
---
+++ src/Form/UserType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email')
+            ->add('roles')
+            ->add('primerNombre')
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'first_options' => ['label' => 'Clave'],
+                'second_options' => ['label' => 'Confirme Clave']
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
