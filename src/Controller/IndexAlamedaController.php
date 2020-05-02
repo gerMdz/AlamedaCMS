7589ef6232eee2dc4a994320896da182b694d1fc
Gerardo J. Montivero <gerardo.montivero@gmail.com>
2020-05-02 07:24:11 -0300

    hasta granted_admin .AlamedaCMS

--- src/Controller/IndexAlamedaController.php
+++ src/Controller/IndexAlamedaController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\IndexAlameda;
 use App\Form\IndexAlamedaType;
 use App\Repository\IndexAlamedaRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,6 +13,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * @Route("/index/alameda")
+ * @IsGranted('ROLE_ADMIN')
  */
 class IndexAlamedaController extends AbstractController
 {
