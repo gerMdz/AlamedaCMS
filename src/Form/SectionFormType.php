5c5da3a3314bcd458c675b0d84d5a107507d9829
Gerardo J. Montivero <gerardo.montivero@gmail.com>
2020-07-13 08:37:55 -0300

    Forms 1. Iniciando SectionController.php
    Signed-off-by: Gerardo J. Montivero <gerardo.montivero@gmail.com>

---
+++ src/Form/SectionFormType.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace App\Form;
+
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class SectionFormType extends AbstractType
+{
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('css')
+            ->add('identificador')
+            ;
+
+    }
+}
