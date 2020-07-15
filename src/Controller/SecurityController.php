a9926eb969ed40064b2092d9ee4de9692457d392
Gerardo J. Montivero <gerardo.montivero@gmail.com>
2020-07-14 22:36:24 -0300

    Upgrade
    Signed-off-by: Gerardo J. Montivero <gerardo.montivero@gmail.com>

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -44,7 +44,7 @@ class SecurityController extends AbstractController
     }
 
     /**
-     * @Route("/logout", name="app_logout")
+     * @Route("/logout/admin", name="app_logout")
      */
     public function logout()
     {
