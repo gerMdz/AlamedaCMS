4f0ff57e00680d8d8ac470c21ddaa788e0446a06
Gerardo J. Montivero <gerardo.montivero@gmail.com>
2020-07-28 08:14:17 -0300

    Empezando form personalizado
    Signed-off-by: Gerardo J. Montivero <gerardo.montivero@gmail.com>

--- src/Form/UserRegistrationFormType.php
+++ src/Form/UserRegistrationFormType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -19,7 +20,7 @@ class UserRegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
+            ->add('email', EmailType::class)
 //            ->add('roles')
             ->add('primerNombre')
             ->add('plainPassword', RepeatedType::class, [
