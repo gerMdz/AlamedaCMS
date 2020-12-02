c4f46b3265be0958943a838b05f36ff2accf63f3
Gerardo J. Montivero <gerardo.montivero@gmail.com>
2020-12-01 21:20:35 -0300

    Metabase admin

--- src/Controller/MetaBaseController.php
+++ src/Controller/MetaBaseController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/metabase")
+ * @Route("/admin/metabase")
  */
 class MetaBaseController extends AbstractController
 {
