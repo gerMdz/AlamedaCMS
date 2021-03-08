11b14e58ad312ef724d0f315d9f906b5373559ed
Gerardo J. Montivero <gerardo.montivero@gmail.com>
2021-03-07 23:25:56 -0300

    Admin de grupo celebración

--- src/Form/GroupCelebrationType.php
+++ src/Form/GroupCelebrationType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\GroupCelebration;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,7 +13,15 @@ class GroupCelebrationType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('isActivo')
+            ->add('isActivo', CheckboxType::class, [
+        'required' => false,
+        'label' => 'Activo?',
+        'label_attr' => ['class' => 'checkbox-custom text-dark'],
+//                'help' => 'Disponible?',
+        'attr' => [
+            'class' => 'form-check-input ',
+        ],
+    ])
             ->add('baseCss')
             ->add('btonCss')
             ->add('imageBg')
