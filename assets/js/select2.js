1756f871357ccc78e7eed75c31caa5dfd0d7a22a
Gerardo J. Montivero <gerardo.montivero@gmail.com>
2021-06-12 18:53:22 -0300

    Intento de select2

--- assets/js/select2.js
+++ assets/js/select2.js
@@ -1,6 +1,7 @@
 import $ from 'jquery';
 import 'select2';                       // globally assign select2 fn to $ element
 import 'select2/dist/css/select2.css';  // optional if you have css loader
+// import "select2-bootstrap4-theme/dist/select2-bootstrap4.min.css";
 
 $(function () {
     $('.select2-enable').select2({
