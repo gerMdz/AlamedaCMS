810cae4ce0e37884d7409e0bb3e6d1ca2f6e72dd
Gerardo J. Montivero <gerardo.montivero@gmail.com>
2021-07-16 18:45:36 -0300

    Modificando formulario de nueva página

--- assets/js/select2-parent.js
+++ assets/js/select2-parent.js
@@ -1,14 +1,7 @@
-import $ from 'jquery';
-import 'select2';                       // globally assign select2 fn to $ element
-import 'select2/dist/css/select2.css';  // optional if you have css loader
+require('../css/app.css');
+const $ = require('jquery');
+console.log('Select2 Parent');
 
-$(function () {
-    $('.select2-enable').select2({
-        placeholder: 'Seleccione item de la lista',
-        dropdownParent: $(".card-body"),
-        allowClear: true
-    });
-
-    console.log('select2-parent')
-});
-
+require('select2')
+require('select2/dist/css/select2.css')
+$('.select2-enable').select2({ width: '100%', placeholder: "Seleccione un item", allowClear: true })
--- webpack.config.js
+++ webpack.config.js
@@ -120,15 +120,15 @@ Encore
 
 
 ;
-if (Encore.isProduction()) {
-    Encore.addPlugin(new PurgeCssPlugin({
-        paths: glob.sync([
-            path.join(__dirname, 'templates/**/*.html.twig')
-        ]),
-        defaultExtractor: (content) => {
-            return content.match(/[\w-/:]+(?<!:)/g) || [];
-        }
-    }));
-}
+// if (Encore.isProduction()) {
+//     Encore.addPlugin(new PurgeCssPlugin({
+//         paths: glob.sync([
+//             path.join(__dirname, 'templates/**/*.html.twig')
+//         ]),
+//         defaultExtractor: (content) => {
+//             return content.match(/[\w-/:]+(?<!:)/g) || [];
+//         }
+//     }));
+// }
 
 module.exports = Encore.getWebpackConfig();
