38df3faea791f111df5899e30da967149cbdb37c
Gerardo J. Montivero <gerardo.montivero@gmail.com>
2022-03-22 20:54:01 -0300

    Corrigiendo menú sm

--- public/material-kit/js/material-kit.js
+++ public/material-kit/js/material-kit.js
@@ -1,7 +1,7 @@
 /*!
 
 =========================================================
-* Material Kit 2 - v3.0.0
+* Material Kit 2 - v3.0.1
 =========================================================
 
 * Product Page: https://www.creative-tim.com/product/soft-ui-design-system
