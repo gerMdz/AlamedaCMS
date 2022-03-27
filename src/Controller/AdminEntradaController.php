071d3e74d2904f84a2b6ad22c9292290b3defd40
Gerardo J. Montivero <gerardo.montivero@gmail.com>
2022-03-26 21:23:13 -0300

    EntradaController optimizando

--- src/Controller/AdminEntradaController.php
+++ src/Controller/AdminEntradaController.php
@@ -17,6 +17,7 @@ use App\Service\ObtenerDatosHelper;
 use App\Service\UploaderHelper;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Query\QueryException;
+use Doctrine\Persistence\ManagerRegistry;
 use Exception;
 use Knp\Component\Pager\PaginatorInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
@@ -31,16 +32,22 @@ class AdminEntradaController extends AbstractController
 {
     private LoggerClient $loggerClient;
     private BoleanToDateHelper $boleanToDateHelper;
+    private ManagerRegistry $managerRegistry;
 
     /**
      * NO usado es opcional.
      * @param LoggerClient $loggerClient
      * @param BoleanToDateHelper $boleanToDateHelper
+     * @param ManagerRegistry $managerRegistry
      */
-    public function __construct(LoggerClient $loggerClient, BoleanToDateHelper $boleanToDateHelper)
-    {
+    public function __construct(
+        LoggerClient $loggerClient,
+        BoleanToDateHelper $boleanToDateHelper,
+        ManagerRegistry $managerRegistry
+    ) {
         $this->loggerClient = $loggerClient;
         $this->boleanToDateHelper = $boleanToDateHelper;
+        $this->managerRegistry = $managerRegistry;
     }
 
     /**
@@ -51,8 +58,11 @@ class AdminEntradaController extends AbstractController
      * @param Request $request
      * @return Response
      */
-    public function index(EntradaRepository $entradaRepository, PaginatorInterface $paginator, Request $request): Response
-    {
+    public function index(
+        EntradaRepository $entradaRepository,
+        PaginatorInterface $paginator,
+        Request $request
+    ): Response {
         $bus = $request->get('busq');
         if ($this->isGranted('ROLE_EDITOR')) {
             $entrada = $entradaRepository->queryFindAllEntradas($bus);
@@ -81,7 +91,8 @@ class AdminEntradaController extends AbstractController
      */
     public function listadoPublicado(EntradaRepository $entradaRepository): Response
     {
-        $this->isGranted('ROLE_EDITOR') ? $entrada = $entradaRepository->findAllPublicadosOrderedByPublicacion() : $entrada = $entradaRepository->findAllPublicadosOrderedByPublicacion($this->getUser());
+        $this->isGranted('ROLE_EDITOR') ? $entrada = $entradaRepository->findAllPublicadosOrderedByPublicacion(
+        ) : $entrada = $entradaRepository->findAllPublicadosOrderedByPublicacion($this->getUser());
 
         return $this->render('admin_entrada/list.html.twig', [
             'entradas' => $entrada,
@@ -93,7 +104,6 @@ class AdminEntradaController extends AbstractController
      * @param Entrada $entrada
      * @param UploaderHelper $uploaderHelper
      * @param ObtenerDatosHelper $datosHelper
-     * @param PrincipalRepository $principalRepository
      * @return RedirectResponse
      * @throws Exception
      * @Route("/admin/entrada/{id}/edit", name="admin_entrada_edit")
@@ -103,10 +113,8 @@ class AdminEntradaController extends AbstractController
         Request $request,
         Entrada $entrada,
         UploaderHelper $uploaderHelper,
-        ObtenerDatosHelper $datosHelper,
-        PrincipalRepository $principalRepository
-    ): Response
-    {
+        ObtenerDatosHelper $datosHelper
+    ): Response {
         $form = $this->createForm(EntradaType::class, $entrada);
         $form->handleRequest($request);
 
@@ -115,7 +123,6 @@ class AdminEntradaController extends AbstractController
             $uploadedFile = $form['imageFile']->getData();
             $boolean = $form['publicar']->getData();
             $link = $form['linkRoute']->getData();
-
 
             $publicado = $this->boleanToDateHelper->setDatatimeForTrue($boolean);
             $entrada->setPublicadoAt($publicado);
@@ -127,9 +134,9 @@ class AdminEntradaController extends AbstractController
                 $entrada->setImageFilename($newFilename);
             }
 
-            $this->getDoctrine()->getManager()->flush();
+            $this->managerRegistry->getManager()->flush();
 
-            $this->loggerClient->logMessage('Se editó la entrada \"' . $entrada->getTitulo() . '\"', '');
+            $this->loggerClient->logMessage('Se editó la entrada \"'.$entrada->getTitulo().'\"', '');
 
             return $this->redirectToRoute('admin_entrada_index');
         }
@@ -158,10 +165,8 @@ class AdminEntradaController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-
-            $this->getDoctrine()->getManager()->flush();
-
-            $this->loggerClient->logMessage('Se editó la entrada \"' . $entrada->getTitulo() . '\"', '');
+            $this->managerRegistry->getManager()->flush();
+            $this->loggerClient->logMessage('Se editó la entrada \"'.$entrada->getTitulo().'\"', '');
 
             return $this->redirectToRoute('admin_entrada_index');
         }
@@ -202,12 +207,11 @@ class AdminEntradaController extends AbstractController
 
             $titulo = $form['titulo']->getData();
 
-            if(!$link and $titulo) {
-                $link = $this->limpiaLink(null,$titulo);
-            }else{
+            if (!$link and $titulo) {
+                $link = $this->limpiaLink($titulo);
+            } else {
                 $link = $link->getlinkRoute();
             }
-
 
             $entrada->setLinkRoute($link);
 
@@ -217,7 +221,6 @@ class AdminEntradaController extends AbstractController
             }
 
             $boolean = $form['publicar']->getData();
-
 
             $publicado = $this->boleanToDateHelper->setDatatimeForTrue($boolean);
             $entrada->setPublicadoAt($publicado);
@@ -235,6 +238,7 @@ class AdminEntradaController extends AbstractController
             'entrada' => $entrada,
         ]);
     }
+
     /**
      * @Route("/admin/new/step1", name="admin_entrada_new_step1", methods={"GET","POST"})
      * @param Request $request
@@ -251,12 +255,11 @@ class AdminEntradaController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $section = $form['section']->getData();
             $entrada->addSection($section);
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($entrada);
-            $entityManager->flush();
+            $this->managerRegistry->getManager()->persist($entrada);
+            $this->managerRegistry->getManager()->flush();
 
             return $this->redirectToRoute('admin_entrada_new_step2', [
-                'id' => $entrada->getId()
+                'id' => $entrada->getId(),
             ]);
         }
 
@@ -265,7 +268,6 @@ class AdminEntradaController extends AbstractController
             'entradaForm' => $form->createView(),
         ]);
     }
-
 
     /**
      * @Route("/admin/new/step2/{id}", name="admin_entrada_new_step2", methods={"GET","POST"})
@@ -276,16 +278,15 @@ class AdminEntradaController extends AbstractController
      */
     public function newStepTwo(Request $request, Entrada $entrada): Response
     {
-
         $form = $this->createForm(StepTwoType::class, $entrada);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
 
-            $this->getDoctrine()->getManager()->flush();
+            $this->managerRegistry->getManager()->flush();
 
             return $this->redirectToRoute('admin_entrada_new_step3', [
-                'id' => $entrada->getId()
+                'id' => $entrada->getId(),
             ]);
         }
 
@@ -311,12 +312,11 @@ class AdminEntradaController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $section = $form['section']->getData();
             $entrada->addSection($section);
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($entrada);
-            $entityManager->flush();
+            $this->managerRegistry->getManager()->persist($entrada);
+            $this->managerRegistry->getManager()->flush();
 
             return $this->redirectToRoute('admin_entrada_index', [
-                'id' => $entrada->getId()
+                'id' => $entrada->getId(),
             ]);
         }
 
@@ -325,7 +325,6 @@ class AdminEntradaController extends AbstractController
             'entradaForm' => $form->createView(),
         ]);
     }
-
 
     /**
      * @Route("/admin/entrada/{linkRoute}", name="entrada_admin_link")
@@ -347,9 +346,6 @@ class AdminEntradaController extends AbstractController
      */
     public function show(Entrada $entrada): Response
     {
-        if (!$entrada) {
-            throw $this->createNotFoundException(sprintf('No se encontró la entrada "%s"', $entrada));
-        }
 
         return $this->render('entrada/show.html.twig', [
             'entrada' => $entrada,
@@ -364,32 +360,24 @@ class AdminEntradaController extends AbstractController
      */
     public function delete(Request $request, Entrada $entrada): Response
     {
-        if ($this->isCsrfTokenValid('delete' . $entrada->getId(), $request->request->get('_token'))) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->remove($entrada);
-            $entityManager->flush();
+        if ($this->isCsrfTokenValid('delete'.$entrada->getId(), $request->request->get('_token'))) {
+            $this->managerRegistry->getManager()->remove($entrada);
+            $this->managerRegistry->getManager()->flush();
         }
 
         return $this->redirectToRoute('admin_entrada_index');
     }
 
     /**
-     * @param null|string $link
      * @param string $titulo
      * @return string
      */
-    private function limpiaLink(?string $link , string $titulo): string
+    private function limpiaLink(string $titulo): string
     {
-        if ('' != $link) {
-            $link = strtolower(str_replace(' ', '-', trim($link)));
-        } else {
-            $link = strtolower(str_replace(' ', '-', trim($titulo)));
-            $link = strtolower(str_replace('<p>', '', trim($link)));
-            $link = strtolower(str_replace('</p>', '', trim($link)));
-        }
-        return $link;
+        $link = strtolower(str_replace(' ', '-', trim($titulo)));
+        $link = strtolower(str_replace('<p>', '', trim($link)));
+        return strtolower(str_replace('</p>', '', trim($link)));
     }
-
 
     /**
      * @param Principal|null $principal
@@ -397,7 +385,7 @@ class AdminEntradaController extends AbstractController
      */
     private function getLinkRoute(?Principal $principal): ?string
     {
-        if(null === $principal){
+        if (null === $principal) {
             return null;
         }
         return $principal->getLinkRoute();
@@ -410,7 +398,7 @@ class AdminEntradaController extends AbstractController
      */
     private function getPrincipal(?string $linkRoute, PrincipalRepository $principalRepository): ?Principal
     {
-        if(null === $linkRoute){
+        if (null === $linkRoute) {
             return null;
         }
 
