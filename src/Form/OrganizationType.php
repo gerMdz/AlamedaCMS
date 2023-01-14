e99ed5897443b05d02ab16e17244273ab34dea6b
Gerardo J. Montivero <gerardo.montivero@gmail.com>
2023-01-14 16:43:34 -0300

    :art: Creando crud de organización

---
+++ src/Form/OrganizationType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Organization;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class OrganizationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('address1')
+            ->add('address2')
+            ->add('identifier')
+            ->add('responsable')
+            ->add('owner')
+            ->add('email')
+            ->add('isActive')
+            ->add('contact')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Organization::class,
+        ]);
+    }
+}
