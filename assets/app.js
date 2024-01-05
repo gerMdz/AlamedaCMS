bcb7a0a73d4dabc6fc4a19891f3a2513fc5a5ca7
germdz <gerardo.montivero@gmail.com>
2024-01-05 16:40:32 -0300

    update recipes symfony web encore

--- assets/app.js
+++ assets/app.js
@@ -5,6 +5,20 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.css';
+
+// start the Stimulus application
+import './bootstrap';
+
+
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
 // any CSS you require will output into a single css file (app.css in this case)
 require('../css/app.css');
 require('../css/igles.css');
