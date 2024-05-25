35f8e4ab26ea3bdd236d3a269bb803a55afc2305
germdz <gerardo.montivero@gmail.com>
2024-05-25 10:49:47 -0300

    Aniversario 98

--- assets/js/select_section.js
+++ assets/js/select_section.js
@@ -3,24 +3,17 @@
 // import 'select2/dist/css/select2.css';  // optional if you have css loader
 // require('../css/app.css');
 const $ = require('jquery');
-require('selectize');
-require('selectize/dist/css/selectize.css')
+require('@selectize/selectize');
+require('@selectize/selectize/dist/css/selectize.css');
 
 
 $(function () {
     $('#entrada_section_section').selectize({
         create: true,
-        sortField: "text",
+        sortField: 'text'
     });
 
 
-    console.log('select2Modal')
 
-    // $("#modalPlantillaWeb").on('shown.bs.modal', function (e) {
-    //         $(this).find('.select2-modal').select2({
-    //             dropdownParent: $(this).find('.modal-body'),
-    //             tags: true
-    //         });
-    //     });
 });
 
