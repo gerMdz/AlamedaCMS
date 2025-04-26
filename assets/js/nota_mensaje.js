6e0fd6ec4f0cf45121f0241c91202e1d9c744263
germdz <gerardo.montivero@mail.com>
2025-04-26 13:12:45 -0300

    Amo mi iglesia 01

--- assets/js/nota_mensaje.js
+++ assets/js/nota_mensaje.js
@@ -8,7 +8,7 @@ const claves = [
 
 $(function () {
     claves.forEach(clave => {
-        const valor = localStorage.getItem(`tocar07P${clave}`);
+        const valor = localStorage.getItem(`ami01P${clave}`);
         const $input = $(`#p${clave}`);
         const $label = $(`#lp${clave}`);
         const $container = $(`#sinp${clave}`);
@@ -46,11 +46,11 @@ $(function () {
 
 function procesa(clave) {
     const val = $(`#p${clave}`).val();
-    localStorage.setItem(`tocar07P${clave}`, JSON.stringify(val));
+    localStorage.setItem(`ami01P${clave}`, JSON.stringify(val));
 }
 
 function aceptado(clave) {
-    const val = JSON.parse(localStorage.getItem(`tocar07P${clave}`));
+    const val = JSON.parse(localStorage.getItem(`ami01P${clave}`));
     const $label = $(`#lp${clave}`);
     const $container = $(`#sinp${clave}`);
     $label.addClass('fontTahu fa-2x text-info').html(val);
