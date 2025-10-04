114d1171dbaa1eaa5b5bf1f3a45a0bf3e830f987
GerMdz <gerardo.montivero@gmail.com>
2025-10-04 10:55:41 -0300

    vtll_personalidad

--- assets/js/nota_mensaje.js
+++ assets/js/nota_mensaje.js
@@ -8,7 +8,7 @@ const claves = [
 
 $(function () {
     claves.forEach(clave => {
-        const valor = localStorage.getItem(`vl09P${clave}`);
+        const valor = localStorage.getItem(`vl10P${clave}`);
         const $input = $(`#p${clave}`);
         const $label = $(`#lp${clave}`);
         const $container = $(`#sinp${clave}`);
@@ -46,11 +46,11 @@ $(function () {
 
 function procesa(clave) {
     const val = $(`#p${clave}`).val();
-    localStorage.setItem(`vl09P${clave}`, JSON.stringify(val));
+    localStorage.setItem(`vl10P${clave}`, JSON.stringify(val));
 }
 
 function aceptado(clave) {
-    const val = JSON.parse(localStorage.getItem(`vl09P${clave}`));
+    const val = JSON.parse(localStorage.getItem(`vl10P${clave}`));
     const $label = $(`#lp${clave}`);
     const $container = $(`#sinp${clave}`);
     $label.addClass('fontTahu fa-2x text-info').html(val);
