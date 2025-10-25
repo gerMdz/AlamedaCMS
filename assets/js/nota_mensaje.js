cce62c791aca4e4b929057fdf49033b14202e8bd
GerMdz <gerardo.montivero@gmail.com>
2025-10-25 20:38:29 -0300

    Ejerce tu autoridad.

--- assets/js/nota_mensaje.js
+++ assets/js/nota_mensaje.js
@@ -8,7 +8,7 @@ const claves = [
 
 $(function () {
     claves.forEach(clave => {
-        const valor = localStorage.getItem(`vl12P${clave}`);
+        const valor = localStorage.getItem(`vl13P${clave}`);
         const $input = $(`#p${clave}`);
         const $label = $(`#lp${clave}`);
         const $container = $(`#sinp${clave}`);
@@ -46,11 +46,11 @@ $(function () {
 
 function procesa(clave) {
     const val = $(`#p${clave}`).val();
-    localStorage.setItem(`vl12P${clave}`, JSON.stringify(val));
+    localStorage.setItem(`vl13P${clave}`, JSON.stringify(val));
 }
 
 function aceptado(clave) {
-    const val = JSON.parse(localStorage.getItem(`vl12P${clave}`));
+    const val = JSON.parse(localStorage.getItem(`vl13P${clave}`));
     const $label = $(`#lp${clave}`);
     const $container = $(`#sinp${clave}`);
     $label.addClass('fontTahu fa-2x text-info').html(val);
