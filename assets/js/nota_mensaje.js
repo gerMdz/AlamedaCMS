5fbcdb5e8b829e7cb992d887d38ecf1d1c6331b2
GerMdz <gerardo.montivero@gmail.com>
2026-03-07 19:58:04 -0300

    Contextual adjustments for notas and localStorage keys.

--- assets/js/nota_mensaje.js
+++ assets/js/nota_mensaje.js
@@ -9,7 +9,7 @@ const claves = [
 
 $(function () {
     claves.forEach(clave => {
-        const valor = localStorage.getItem(`dat04P${clave}`);
+        const valor = localStorage.getItem(`v2601P${clave}`);
         const $input = $(`#p${clave}`);
         const $label = $(`#lp${clave}`);
         const $container = $(`#sinp${clave}`);
@@ -47,11 +47,11 @@ $(function () {
 
 function procesa(clave) {
     const val = $(`#p${clave}`).val();
-    localStorage.setItem(`dat04P${clave}`, JSON.stringify(val));
+    localStorage.setItem(`v2601P${clave}`, JSON.stringify(val));
 }
 
 function aceptado(clave) {
-    const val = JSON.parse(localStorage.getItem(`dat04P${clave}`));
+    const val = JSON.parse(localStorage.getItem(`v2601P${clave}`));
     const $label = $(`#lp${clave}`);
     const $container = $(`#sinp${clave}`);
     $label.addClass('fontTahu fa-2x text-info').html(val);
