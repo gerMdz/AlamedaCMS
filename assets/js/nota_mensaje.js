a5322a1c74f09b295a3282b960747c2dd562a8f5
GerMdz <gerardo.montivero@gmail.com>
2026-08-09 13:51:56 -0300

    Actualizado contenido en `_nota.html.twig` para centrarse en el dominio propio, reemplazando referencias de mansedumbre, con citas bíblicas revisadas. Modificado prefijo de claves en `nota_mensaje.js` a `tmv09P` para alinear con la nueva nomenclatura.

--- assets/js/nota_mensaje.js
+++ assets/js/nota_mensaje.js
@@ -9,7 +9,7 @@ const claves = [
 
 $(function () {
     claves.forEach(clave => {
-        const valor = localStorage.getItem(`tmv08P${clave}`);
+        const valor = localStorage.getItem(`tmv09P${clave}`);
         const $input = $(`#p${clave}`);
         const $label = $(`#lp${clave}`);
         const $container = $(`#sinp${clave}`);
@@ -47,11 +47,11 @@ $(function () {
 
 function procesa(clave) {
     const val = $(`#p${clave}`).val();
-    localStorage.setItem(`tmv08P${clave}`, JSON.stringify(val));
+    localStorage.setItem(`tmv09P${clave}`, JSON.stringify(val));
 }
 
 function aceptado(clave) {
-    const val = JSON.parse(localStorage.getItem(`tmv08P${clave}`));
+    const val = JSON.parse(localStorage.getItem(`tmv09P${clave}`));
     const $label = $(`#lp${clave}`);
     const $container = $(`#sinp${clave}`);
     $label.addClass('fontTahu fa-2x text-info').html(val);
